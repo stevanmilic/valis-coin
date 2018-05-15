5a50671858526a3a8bca31e836e91f28226ae54b
Stevan Milic <stevan.milic@yahoo.com>
2018-05-15 23:37:08 +0200

    Add  class for serialization

--- blockchain/model_types.py
+++ blockchain/model_types.py
@@ -3,13 +3,22 @@ from apistar import types, validators
 from blockchain.blocks import get_block
 
 
-class Transaction(types.Type):
+class PickleType(types.Type):
+
+    def __getstate__(self):
+        return self._dict
+
+    def __setstate__(self, _dict):
+        object.__setattr__(self, '_dict', _dict)
+
+
+class Transaction(PickleType):
     sender = validators.String(max_length=100, allow_null=True)
     receiver = validators.String(max_length=100)
     amount = validators.Integer()
 
 
-class Block(types.Type):
+class Block(PickleType):
     height = validators.Integer()
     timestamp = validators.DateTime()
     txns = validators.Array(items=Transaction, allow_null=True, default=[])
@@ -22,11 +31,11 @@ class Block(types.Type):
         return get_block(self.previous_hash)
 
 
-class Node(types.Type):
+class Node(PickleType):
     address = validators.String(max_length=100)
     # last_seen = validators.DateTime()
     # TBD^
 
 
-class BlockInfo(types.Type):
+class BlockInfo(PickleType):
     height = validators.Integer(allow_null=True)
